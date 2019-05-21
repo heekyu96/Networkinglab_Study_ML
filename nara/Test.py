3677771ee011ace25799ec3093d28668aa276c92
heegi <imtt96@icloud.com>
2019-05-21 14:50:34 +0900

    Test_heegi

--- nara/Test.py
+++ nara/Test.py
@@ -20,8 +20,9 @@ print(Q)
 # Q = np.random.normal(loc=0.1, scale=1, size=(env.observation_space.n, env.action_space.n))
 dis = 0.95
 r = 0.6
-num_episodes = 10000
+num_episodes = 4000
 rList = []
+for_graph = []
 
 for i in range(num_episodes):
     state = env.reset()
@@ -44,10 +45,12 @@ for i in range(num_episodes):
         state = new_state
 
     rList.append(rAll)
+    for_graph.append((sum(rList) / (i + 1) * 100))
 
-print("Success rate : " + str(sum(rList) / num_episodes))
+print("Success rate : " + str(sum(rList) / num_episodes * 100))
 print("Final Q-Table Values")
 print(Q)
 
-plt.bar(range(len(rList)), rList, color="black")
+# plt.bar(range(len(rList)), rList, color="black")
+plt.plot(range(len(rList)), for_graph, linewidth=1.0, color="black")
 plt.show()
